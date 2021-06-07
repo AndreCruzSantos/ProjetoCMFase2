74f90210bb36d1638dbd278b602a5d3458c1896b
João Bandeira <190221045@estudantes.ips.pt>
2021-06-07 18:33:56 +0100

    Criar Evento quase completo

--- App.js
+++ App.js
@@ -1,112 +1,188 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-   
-import React from 'react';
-import type {Node} from 'react';
+
+import React, { Component, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  TextInput,
+  Text,
+  Animated,
+  Switch,
+  TouchableOpacity,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import DateTimePicker from "react-native-modal-datetime-picker";
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+class FloatingLabelInput extends Component {
+  state = {
+    isFocused: false,
+  }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  UNSAFE_componentWillMount(){
+    this._animatedIsFocused = new Animated.Value(this.props.value === '' ? 0 : 1);
+  }
+
+  handleFocus = () => this.setState({ isFocused: true });
+  handleBlur = () => this.setState({ isFocused: false });
+
+  componentDidUpdate(){
+    Animated.timing(this._animatedIsFocused, {
+      toValue: (this.state.isFocused ? 1 : 0 || this.props.value !== '') ? 1 : 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  }
+
+  render() {
+    const { label, ...props } = this.props;
+    const { isFocused } = this.state;
+    const labelStyle = {
+      position: 'absolute',
+      left: 0,
+      top: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: [18,0],
+      }),
+      fontSize: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: [20,14],
+      }),
+      color: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: ['#aaa','#aaa'],
+      }),
+    };
+    return (
+      <View style={{ paddingTop: 18}}>
+        <Animated.Text style={labelStyle}>
+          {label}
+        </Animated.Text>
+        <TextInput 
+          {...props}
+          style={{ height: 39, fontSize: 15, color: '#aaa', borderBottomWidth: 1, borderBottomColor: '#555' }}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+          blurOnSubmit
+          //onSubmitEditing={console.log(this.props.value)}
+        />
+
+        
+      </View>
+    );
+  }
+}
+
+var styles = {
+  
+  switchView: {
+    marginLeft:20, 
+    flexDirection:'row', 
+    justifyContent:'space-between'
+  },
+  dataView: {
+    marginLeft:20, 
+    flexDirection:'row', 
+    justifyContent:'space-between'
+  },
+}
+
+const Example = () => {
+  
+  const state = {
+    value: '',
+    description: '',
+    location: '',
+    isEnabled: false,
+    startDate: new Date(),
+    isVisible: false
+  }
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isEnabled, setEnabled] = useState(false);
+  const [value, handleTextChange] = useState('');
+  const [description, handleDescriptionChange] = useState('');
+  const [location, handleLocationChange] = useState('');
+  
+  const toggleEnable = () => setEnabled(!isEnabled)
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+
+    //atribuir o valor date a uma variavel
+    hideDatePicker();
+  };
+
+  const handleTitleChange = (newText) => handleTextChange(newText)
+  const handleDescriptionChangeText = (newText) => handleDescriptionChange(newText)
+  const handleLocationChangeText = (newText) => handleLocationChange(newText)
+  
+
+    return (
+      <View>
+
+        <View style={{ margin: 20 }}>
+          
+          <FloatingLabelInput
+            label="Título"
+            value={value}
+            onChangeText={handleTitleChange}
+            
+          />
+
+          <FloatingLabelInput
+            label="Descrição"
+            value={description}
+            onChangeText={handleDescriptionChangeText}
+          />
+
+          <FloatingLabelInput
+            label="Localização"
+            value={location}
+            onChangeText={handleLocationChangeText}
+          />
+        
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
-export default App;
+        <View style={styles.switchView}>
+          <Text style={{color:"#aaa", fontSize: 20, }}>
+            All day 
+          </Text>
+          <Switch style={{marginEnd:'4%'}}
+            onValueChange={toggleEnable}
+            value={isEnabled}
+          >
+          </Switch>
+        </View>
+        
+
+        <View style={{marginTop: 20}}>
+           <TouchableOpacity style={styles.dataView}
+           onPress={showDatePicker}>
+             <Text style={{color:"#aaa", fontSize: 20}}>Início </Text>
+             <Text style={{color:"#aaa", fontSize: 15, marginEnd: '4%'}}> {state.startDate.toDateString()}</Text>
+           </TouchableOpacity>
+
+            <DateTimePicker
+              isVisible={isDatePickerVisible}
+              mode="date"
+              display="spinner"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+            />
+
+        </View>
+ 
+      </View>
+    );
+  
+}
+
+export default Example;
