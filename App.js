da4841100052d076c6311cb6ad7d09f67b646a76
João Bandeira <190221045@estudantes.ips.pt>
2021-06-08 15:33:39 +0100

    Criar Evento com DataPicker e na pasta certa

--- App.js
+++ App.js
@@ -1,188 +1,20 @@
 
-import React, { Component, useState } from 'react';
-import {
-  View,
-  TextInput,
-  Text,
-  Animated,
-  Switch,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import CreateEvent from './screens/create_event';
 
-import DateTimePicker from "react-native-modal-datetime-picker";
+const Stack = createStackNavigator();
 
-class FloatingLabelInput extends Component {
-  state = {
-    isFocused: false,
-  }
-
-
-  UNSAFE_componentWillMount(){
-    this._animatedIsFocused = new Animated.Value(this.props.value === '' ? 0 : 1);
-  }
-
-  handleFocus = () => this.setState({ isFocused: true });
-  handleBlur = () => this.setState({ isFocused: false });
-
-  componentDidUpdate(){
-    Animated.timing(this._animatedIsFocused, {
-      toValue: (this.state.isFocused ? 1 : 0 || this.props.value !== '') ? 1 : 0,
-      duration: 200,
-      useNativeDriver: false,
-    }).start();
-  }
-
-  render() {
-    const { label, ...props } = this.props;
-    const { isFocused } = this.state;
-    const labelStyle = {
-      position: 'absolute',
-      left: 0,
-      top: this._animatedIsFocused.interpolate({
-        inputRange: [0,1],
-        outputRange: [18,0],
-      }),
-      fontSize: this._animatedIsFocused.interpolate({
-        inputRange: [0,1],
-        outputRange: [20,14],
-      }),
-      color: this._animatedIsFocused.interpolate({
-        inputRange: [0,1],
-        outputRange: ['#aaa','#aaa'],
-      }),
-    };
-    return (
-      <View style={{ paddingTop: 18}}>
-        <Animated.Text style={labelStyle}>
-          {label}
-        </Animated.Text>
-        <TextInput 
-          {...props}
-          style={{ height: 39, fontSize: 15, color: '#aaa', borderBottomWidth: 1, borderBottomColor: '#555' }}
-          onFocus={this.handleFocus}
-          onBlur={this.handleBlur}
-          blurOnSubmit
-          //onSubmitEditing={console.log(this.props.value)}
-        />
-
-        
-      </View>
-    );
-  }
+function App(){
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Criar Evento">
+        <Stack.Screen name="Criar Evento" component={CreateEvent}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-var styles = {
-  
-  switchView: {
-    marginLeft:20, 
-    flexDirection:'row', 
-    justifyContent:'space-between'
-  },
-  dataView: {
-    marginLeft:20, 
-    flexDirection:'row', 
-    justifyContent:'space-between'
-  },
-}
+export default App;
 
-const Example = () => {
-  
-  const state = {
-    value: '',
-    description: '',
-    location: '',
-    isEnabled: false,
-    startDate: new Date(),
-    isVisible: false
-  }
-
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [isEnabled, setEnabled] = useState(false);
-  const [value, handleTextChange] = useState('');
-  const [description, handleDescriptionChange] = useState('');
-  const [location, handleLocationChange] = useState('');
-  
-  const toggleEnable = () => setEnabled(!isEnabled)
-
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date) => {
-    console.warn("A date has been picked: ", date);
-
-    //atribuir o valor date a uma variavel
-    hideDatePicker();
-  };
-
-  const handleTitleChange = (newText) => handleTextChange(newText)
-  const handleDescriptionChangeText = (newText) => handleDescriptionChange(newText)
-  const handleLocationChangeText = (newText) => handleLocationChange(newText)
-  
-
-    return (
-      <View>
-
-        <View style={{ margin: 20 }}>
-          
-          <FloatingLabelInput
-            label="Título"
-            value={value}
-            onChangeText={handleTitleChange}
-            
-          />
-
-          <FloatingLabelInput
-            label="Descrição"
-            value={description}
-            onChangeText={handleDescriptionChangeText}
-          />
-
-          <FloatingLabelInput
-            label="Localização"
-            value={location}
-            onChangeText={handleLocationChangeText}
-          />
-        
-        </View>
-
-
-        <View style={styles.switchView}>
-          <Text style={{color:"#aaa", fontSize: 20, }}>
-            All day 
-          </Text>
-          <Switch style={{marginEnd:'4%'}}
-            onValueChange={toggleEnable}
-            value={isEnabled}
-          >
-          </Switch>
-        </View>
-        
-
-        <View style={{marginTop: 20}}>
-           <TouchableOpacity style={styles.dataView}
-           onPress={showDatePicker}>
-             <Text style={{color:"#aaa", fontSize: 20}}>Início </Text>
-             <Text style={{color:"#aaa", fontSize: 15, marginEnd: '4%'}}> {state.startDate.toDateString()}</Text>
-           </TouchableOpacity>
-
-            <DateTimePicker
-              isVisible={isDatePickerVisible}
-              mode="date"
-              display="spinner"
-              onConfirm={handleConfirm}
-              onCancel={hideDatePicker}
-            />
-
-        </View>
- 
-      </View>
-    );
-  
-}
-
-export default Example;
--- screens/create_event.js
+++ screens/create_event.js
@@ -1,64 +1,231 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import {
   View,
-  StatusBar,
   TextInput,
   Text,
+  Animated,
+  Switch,
+  TouchableOpacity,
 } from 'react-native';
+
+import DateTimePicker from "react-native-modal-datetime-picker";
+import Moment from 'moment';
+import MomentPT from 'moment/src/locale/pt';
+
 
 class FloatingLabelInput extends Component {
   state = {
     isFocused: false,
-  };
+  }
+
+
+  UNSAFE_componentWillMount(){
+    this._animatedIsFocused = new Animated.Value(this.props.value === '' ? 0 : 1);
+  }
 
   handleFocus = () => this.setState({ isFocused: true });
   handleBlur = () => this.setState({ isFocused: false });
 
+  componentDidUpdate(){
+    Animated.timing(this._animatedIsFocused, {
+      toValue: (this.state.isFocused ? 1 : 0 || this.props.value !== '') ? 1 : 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  }
+
   render() {
+
     const { label, ...props } = this.props;
     const { isFocused } = this.state;
     const labelStyle = {
       position: 'absolute',
       left: 0,
-      top: !isFocused ? 18 : 0,
-      fontSize: !isFocused ? 20 : 14,
-      color: !isFocused ? '#aaa' : '#000',
+      top: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: [18,0],
+      }),
+      fontSize: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: [20,14],
+      }),
+      color: this._animatedIsFocused.interpolate({
+        inputRange: [0,1],
+        outputRange: ['#000','#000'],
+      }),
     };
     return (
-      <View style={{ paddingTop: 18 }}>
-        <Text style={labelStyle}>
+      <View style={{ paddingTop: 18}}>
+        <Animated.Text style={labelStyle}>
           {label}
-        </Text>
-        <TextInput
+        </Animated.Text>
+        <TextInput 
           {...props}
-          style={{ height: 26, fontSize: 20, color: '#000', borderBottomWidth: 1, borderBottomColor: '#555' }}
+          style={{ height: 39, fontSize: 15, color: '#000', borderBottomWidth: 1, borderBottomColor: '#555' }}
           onFocus={this.handleFocus}
           onBlur={this.handleBlur}
           blurOnSubmit
+          //onSubmitEditing={console.log(this.props.value)}
         />
+
+        
       </View>
     );
   }
 }
 
+var styles = {
+  
+  switchView: {
+    marginLeft:20, 
+    flexDirection:'row', 
+    justifyContent:'space-between'
+  },
+  dataView: {
+    marginLeft:20, 
+    flexDirection:'row', 
+    justifyContent:'space-between'
+  },
+}
 
-export default class App extends Component {
-  state = {
+const CreateEvent = () => {
+  
+  const state = {
     value: '',
+    description: '',
+    location: '',
+    isEnabled: false,
+    startDate: new Date(),
+    isVisible: false,
+    endDate: new Date()
+  }
+  
+  //Moment.defineLocale('pt', MomentPT);
+  
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isEndDatePickerVisible, setEndDatePickerVisibility] = useState(false);
+  const [isEnabled, setEnabled] = useState(false);
+  const [value, handleTextChange] = useState('');
+  const [description, handleDescriptionChange] = useState('');
+  const [location, handleLocationChange] = useState('');
+  
+  const [startDate, setStartDate] = useState(new Date());
+
+  const [endDate, setEndDate] = useState(new Date());
+  
+  const toggleEnable = () => setEnabled(!isEnabled)
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
   };
 
-  handleTextChange = (newText) => this.setState({ value: newText });
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
 
-  render() {
+  const showEndDatePicker = () => {
+    setEndDatePickerVisibility(true);
+  };
+
+  const hideEndDatePicker = () => {
+    setEndDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    setStartDate(date);
+    hideDatePicker();
+  };
+
+  const handleEndConfirm = (date) => {
+    setEndDate(date);
+    hideEndDatePicker();
+  };
+
+  const handleTitleChange = (newText) => handleTextChange(newText)
+  const handleDescriptionChangeText = (newText) => handleDescriptionChange(newText)
+  const handleLocationChangeText = (newText) => handleLocationChange(newText)
+  
+
     return (
-      <View style={{ flex: 1, padding: 30, backgroundColor: '#f5fcff' }}>
-        <StatusBar hidden />
-        <FloatingLabelInput
-          label="Email"
-          value={this.state.value}
-          onChangeText={this.handleTextChange}
-        />
+      
+      <View>
+
+        <View style={{ margin: 20 }}>
+          
+          <FloatingLabelInput
+            label="Título"
+            value={value}
+            onChangeText={handleTitleChange}
+            
+          />
+
+          <FloatingLabelInput
+            label="Descrição"
+            value={description}
+            onChangeText={handleDescriptionChangeText}
+          />
+
+          <FloatingLabelInput
+            label="Localização"
+            value={location}
+            onChangeText={handleLocationChangeText}
+          />
+        
+        </View>
+
+
+        <View style={styles.switchView}>
+          <Text style={{color:"#000", fontSize: 20}}>
+            O dia todo
+          </Text>
+          <Switch style={{marginEnd:'4%'}}
+            trackColor={{true: 'green', false:'grey'}}
+            onValueChange={toggleEnable}
+            value={isEnabled}
+          >
+          </Switch>
+        </View>
+        
+
+        <View style={{marginTop: 20}}>
+           <TouchableOpacity style={styles.dataView}
+           onPress={showDatePicker}>
+             <Text style={{color:"#000", fontSize: 20}}>Início </Text>
+             
+             <Text style={{color:"#000", fontSize: 15, marginEnd: '4%'}}> {Moment(startDate).format('DD ' + '' + 'MMMM' + ', ' + 'HH:mm' )}</Text>
+           </TouchableOpacity>
+            
+            <DateTimePicker
+              isVisible={isDatePickerVisible}
+              mode="datetime"
+              display="spinner"
+              onConfirm={handleConfirm}
+              onCancel={hideDatePicker}
+            />
+
+        </View>
+
+        <View style={{marginTop: 20}}>
+           <TouchableOpacity style={styles.dataView}
+           onPress={showEndDatePicker}>
+             <Text style={{color:"#000", fontSize: 20}}>Fim </Text>
+             
+             <Text style={{color:"#000", fontSize: 15, marginEnd: '4%'}}> {Moment(endDate).format('DD ' + '' + 'MMMM' + ', ' + 'HH:mm' )}</Text>
+           </TouchableOpacity>
+            
+            <DateTimePicker
+              isVisible={isEndDatePickerVisible}
+              mode="datetime"
+              display="spinner"
+              onConfirm={handleEndConfirm}
+              onCancel={hideEndDatePicker}
+            />
+
+        </View>
+ 
       </View>
     );
-  }
+  
 }
+
+export default CreateEvent;
