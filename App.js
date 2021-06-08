4a20228dde87a9b1a38f6f07f1f5084c65f24368
André Santos <190221049@estudantes.ips.pt>
2021-06-08 15:46:55 +0100

    O melhor Login e Register de sempre PS: Couxo ainda nao fez nada

--- App.js
+++ App.js
@@ -8,175 +8,23 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  useColorScheme,
-  View,
-  Image,
-  Button,
-  TouchableOpacity
-} from 'react-native';
+import Login from "./screens/login.js"
+import Register from "./screens/register.js"
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-function SignInScreen(){
-  return(
-    <View style={styles.background}>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_email_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Email"></TextInput>
-      </View>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_lock_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Password" secureTextEntry={true}></TextInput>
-      </View>
-      <Text style={styles.forgotPasswordTxt}>
-        Esqueceu-se da palavra-passe?
-      </Text>
-      <TouchableOpacity style={styles.signInBtn}>
-        <Text style={styles.signInText}>
-          Iniciar Sessão
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.registerBtn}>
-        <Text style={styles.signInText}>
-          Registar
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
-
-function SignUpScreen(){
-  return(
-    <View style={styles.background}>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_person_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Nome de Utilizador"></TextInput>
-      </View>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_email_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Email"></TextInput>
-      </View>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_lock_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Password"></TextInput>
-      </View>
-      <View style={styles.inputView}>
-        <Image source={require("./images/baseline_lock_black_24dp.png")} style={styles.image}></Image>
-        <TextInput style = {styles.input} placeholder="Repetir Password"></TextInput>
-      </View>
-      <TouchableOpacity style={styles.signInBtn}>
-        <Text style={styles.signInText}>
-          Registe-se
-        </Text>
-      </TouchableOpacity><TouchableOpacity style={styles.registerBtn}>
-        <Text style={styles.signInText}>
-         Já tem conta?
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
 const App: () => Node = () => {
   return (
-    SignInScreen()
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Register">
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Register" component={Register}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  background:{
-    flex: 1,
-    backgroundColor: '#C0C0C0',
-    alignItems: 'center',
-  },
-  inputView: {
-    flexDirection: 'row',
-    width: '70%',
-    marginTop: 10,
-    backgroundColor: 'white',
-    borderRadius: 25,
-  },
-  image:{
-    width:25,
-    height:25,
-    marginTop: 12,
-    marginLeft: 10
-  },
-  input:{
-    flex:1,
-    paddingLeft: 7,
-    fontSize: 16
-  },
-  signInBtn:{
-    width: '70%',
-    height: 40,
-    borderWidth: 1.5,
-    borderColor: '#009900',
-    backgroundColor: '#00CC66',
-    marginTop: 23,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  signInText:{
-    fontSize: 17,
-    fontWeight: 'bold',
-  },
-  forgotPasswordTxt:{
-    fontSize:13,
-    marginTop: 13,
-    marginRight: '15%'
-  },
-  registerBtn:{
-    width: '70%',
-    height: 40,
-    borderWidth: 1.5,
-    borderColor: '#009900',
-    backgroundColor: '#C0C0C0',
-    marginTop: 23,
-    borderRadius: 20,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
 
 export default App;
---
+++ screens/login.js
@@ -0,0 +1,108 @@
+import React from "react";
+
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class Login extends React.Component{
+    render(){
+        return (
+            <View style={styles.background}>
+          <Image source={require("../images/AgendYourself_Logo.png")} style={styles.logo}></Image>
+          <View style={styles.inputView}>
+            <Image source={require("../images/baseline_email_black_24dp.png")} style={styles.image}></Image>
+            <TextInput style = {styles.input} placeholder="Email"></TextInput>
+          </View>
+          <View style={styles.inputView}>
+            <Image source={require("../images/baseline_lock_black_24dp.png")} style={styles.image}></Image>
+            <TextInput style = {styles.input} placeholder="Password" secureTextEntry={true}></TextInput>
+          </View>
+          <Text style={styles.forgotPasswordTxt}>
+            Esqueceu-se da palavra-passe?
+          </Text>
+          <TouchableOpacity style={styles.signInBtn}>
+            <Text style={styles.signInText}>
+              Iniciar Sessão
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.registerBtn}>
+            <Text style={styles.registerTxt}>
+              Registar
+            </Text>
+          </TouchableOpacity>
+        </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+  background:{
+    flex: 1,
+    backgroundColor: '#2B2A2A',
+    alignItems: 'center',
+  },
+  inputView: {
+    flexDirection: 'row',
+    width: '70%',
+    marginTop: 10,
+    backgroundColor: 'white',
+    borderRadius: 25,
+  },
+  image:{
+    width:25,
+    height:25,
+    marginTop: 12,
+    marginLeft: 10
+  },
+  input:{
+    flex:1,
+    paddingLeft: 7,
+    fontSize: 16
+  },
+  signInBtn:{
+    width: '70%',
+    height: 40,
+    borderWidth: 1.5,
+    backgroundColor: '#FF8000',
+    marginTop: 23,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  signInText:{
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'black'
+  },
+  forgotPasswordTxt:{
+    fontSize:13,
+    marginTop: 13,
+    marginRight: '15%',
+    color: 'white'
+  },
+  registerBtn:{
+    width: '70%',
+    height: 40,
+    borderWidth: 1.5,
+    borderColor: '#FF8000',
+    backgroundColor: '#2B2A2A',
+    marginTop: 23,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  logo:{
+    width:230,
+    height:230
+  },
+  registerTxt:{
+    color: 'white',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+});
