c53a89e5c0da46596ee6bbf152c9bc52e654fa8a
Not Kreader <190221046@estudantes.ips.pt>
2021-06-16 23:18:18 +0100

    Map a funcionar

--- screens/map.js
+++ screens/map.js
@@ -4,6 +4,7 @@ import { View, Image, Text, Button, ScrollView, TouchableOpacity, Alert, StyleSh
 import prompt from 'react-native-prompt-android';
 import MapView, { PROVIDER_GOOGLE, Marker, Polyline } from 'react-native-maps';
 import Geolocation from "react-native-geolocation-service";
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default class MapScreen extends React.Component {
@@ -13,7 +14,8 @@ export default class MapScreen extends React.Component {
             latitude: 37.78825,
             longitude: -122.4324,
             latitudeDelta: 0.0922,
-            longitudeDelta: 0.0121
+            longitudeDelta: 0.0121,
+            marker: null
          };
        }
 
@@ -25,6 +27,14 @@ export default class MapScreen extends React.Component {
               latitudeDelta: 0.0922,
               longitudeDelta: 0.0121
             });
+            if (this.map) {
+              this.map.animateToRegion({
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0121
+              })
+            }
           },
           error => {
             Alert.alert(error.message.toString());
@@ -37,33 +47,45 @@ export default class MapScreen extends React.Component {
           }
         );
       }
+
     render() {
 
         return (
             <View style={styles.container}>
                 <MapView
-                    provider={PROVIDER_GOOGLE}
-                    style={styles.map}
-                    mapType={"hybrid"}
-                    showsUserLocation={true}
-                    region={{
-                        latitude: this.state.latitude,
-                        longitude: this.state.longitude,
-                        latitudeDelta: this.state.latitudeDelta,
-                        longitudeDelta: this.state.longitudeDelta
-                    }}>
-                        <MapView.Marker 
-                            draggable
-                            coordinate={{
-                                latitude: this.state.latitude,
-                                longitude: this.state.longitude,
-                            }}
-                            pinColor = {"darkorange"}     
-                            title={"My Location"}
-                            description={"User's current location"}
-                            />
+                  initialRegion={{
+                    latitude: this.state.latitude,
+                    longitude: this.state.longitude,
+                    latitudeDelta: this.state.latitudeDelta,
+                    longitudeDelta: this.state.longitudeDelta
+                  }}
+                  region={{latitude: this.state.latitude,
+                    longitude: this.state.longitude,
+                    latitudeDelta: this.state.latitudeDelta,
+                    longitudeDelta: this.state.longitudeDelta}}
+                  provider={PROVIDER_GOOGLE}
+                  style={styles.map}
+                  mapType={"hybrid"}
+                  showsUserLocation={true}
+                    
+                  onPress={(e) => this.setState({ 
+                    latitude: e.nativeEvent.coordinate.latitude,
+                    longitude: e.nativeEvent.coordinate.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0121,
+                    marker: e.nativeEvent.coordinate })}>
+                  {this.state.marker && <MapView.Marker coordinate={this.state.marker} pinColor={'darkorange'} /> }
                 </MapView>
-                
+                <View style={styles.btnContainer}>
+                  {this.state.marker != null ? 
+                    <Button 
+                      title="Selecionar Localização" 
+                      color="#FF8000"
+                      onPress={() => this.props.navigation.navigate('Criar Evento', {lat: this.state.latitude, long: this.state.longitude})}
+                    ></Button>
+                    : null 
+                  }
+                </View>
             </View>
         );
     }
@@ -77,4 +99,10 @@ const styles = StyleSheet.create({
     map: {
       ...StyleSheet.absoluteFillObject,
     },
+    btnContainer: {
+      position: 'absolute',
+      bottom: 0,
+      marginBottom: 75,
+      alignSelf: 'center'
+    }
    });
